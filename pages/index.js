a17e01e20ec669fb3a6285b116b3e6a30ed99701
Dzikri <dzikri.qalam01@ui.ac.id>
2022-08-10 13:24:16 +0700

    feat: add current about me

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../ui/component/Navbar'
 import Hero from '../ui/component/Hero'
+import About from '../ui/component/About'
 
 export default function Home() {
   return (<>
     <Hero />
+    <About />
   </>)
 }
--- ui/component/About/index.jsx
+++ ui/component/About/index.jsx
@@ -1,9 +1,21 @@
+import { Box, Text, VStack, Heading } from '@chakra-ui/react'
 import React from 'react'
 
-function index() {
+function About() {
     return (
-        <div>index</div>
+        <VStack bg={'#161616'} minH='100vh' justifyContent={'center'}>
+            <Heading fontWeight={700} color={'white'} fontSize={'37.5px'}>
+                ABOUT ME
+            </Heading>
+            <Box bg='brand.green' height={'3px'} width='65px' borderRadius={'10px'} />
+            <Text fontWeight={500} color={'#8F8F8F'} w={'49%'}>
+                I'm Thiago Leão, I'm 20 years old and I live in São Vicente -SP.
+                Front-end developer and UI Designer, I'm passionate about interfaces, from their creation to development. I like to learn more and more about the area, challenges that take me out of my comfort zone are the best.
+
+                I have prototyped and developed Landing Pages, Sites, E-commerces, E-mails Marketing, Apps and Programs.
+            </Text>
+        </VStack>
     )
 }
 
-export default index
+export default About
