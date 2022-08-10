951c1489d46e4d33a03bdcc23d03dff3db663e59
Dzikri <dzikri.qalam01@ui.ac.id>
2022-08-10 14:10:10 +0700

    feat: add my skills

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from '../ui/component/Navbar'
 import Hero from '../ui/component/Hero'
 import About from '../ui/component/About'
+import Skills from '../ui/component/Skills'
 
 export default function Home() {
   return (<>
     <Hero />
     <About />
+    <Skills />
   </>)
 }
---
+++ ui/component/Skills/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Box, Text, VStack, Heading, Image, SimpleGrid, HStack } from '@chakra-ui/react'
+
+function Item({ image, text }) {
+    return (
+        <HStack border='2px solid green' width={'auto'}>
+            <Image src={image} />
+            <Text fontWeight={600} color={'#E2E2E2'} fontSize={'22px'}>
+                {text}
+            </Text>
+        </HStack>
+    )
+}
+
+function Skills() {
+    return (
+        <VStack bg={'#1D1D1D'} minH='100vh' justifyContent={'center'} position={'relative'} overflow='hidden'>
+            <Heading fontWeight={700} color={'white'} fontSize={'32px'} mb='50px'>
+                My Skills
+            </Heading>
+            <SimpleGrid columns={2} spacingX={'300px'} spacingY={'50px'} border='2px solid red' w={'60%'}>
+                <Item image={'/assets/socmed/github.svg'} text={'HTML'} />
+                <Item image={'/assets/socmed/github.svg'} text={'HTML'} />
+                <Item image={'/assets/socmed/github.svg'} text={'HTML'} />
+                <Item image={'/assets/socmed/github.svg'} text={'HTML'} />
+            </SimpleGrid>
+        </VStack>
+    )
+}
+
+export default Skills
