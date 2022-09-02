e0b9c61103bc3a3bfeeceb9dd6f35794c183ffca
Dzikri <dzikri.qalam01@ui.ac.id>
2022-09-03 01:42:37 +0700

    feat: update footer responsiveness

--- ui/component/Footer/index.jsx
+++ ui/component/Footer/index.jsx
@@ -3,8 +3,8 @@ import { Flex, Text } from '@chakra-ui/react'
 
 function Footer() {
     return (
-        <Flex justify={'center'} align='center' py='28px' bg='#202020'>
-            <Text color='#8F8F8F'>
+        <Flex justify={'center'} align='center' py='28px' bg='#202020' px='5%'>
+            <Text color='#8F8F8F' textAlign={'center'}>
                 © 2022 Dzikri Qalam Hatorangan. All rights reserved.
             </Text>
         </Flex>
