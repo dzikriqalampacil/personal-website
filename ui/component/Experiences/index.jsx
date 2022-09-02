90075c8fa6c2f1146450e3f745c20f9ecdef57c2
Dzikri <dzikri.qalam01@ui.ac.id>
2022-09-02 23:17:27 +0700

    feat: handle responsiveness for experience

--- ui/component/Experiences/index.jsx
+++ ui/component/Experiences/index.jsx
@@ -19,14 +19,14 @@ function Item({ date, title, institution }) {
 
 function Experiences() {
     return (
-        <VStack bg={'#161616'} minH='100vh' position={'relative'} overflow='hidden' pb='280px'>
-            <Text fontWeight={700} color={'#272727'} fontSize={'150px'} position={'absolute'} left='-50' bottom='5'>
+        <VStack bg={'#161616'} minH='100vh' position={'relative'} overflow='hidden' pb={{ base: '90px', sm: '120px', md: '280px' }}>
+            <Text fontWeight={700} color={'#272727'} fontSize={{ base: '42px', md: '100px', lg: '150px' }} position={'absolute'} left={{ base: '-4', md: '-10', lg: '-65' }} bottom='5'>
                 UT ME
             </Text>
-            <Image src={'/assets/exballs.svg'} position={'absolute'} right='5' top='5' />
-            <SimpleGrid columns={2} pt='70px' spacingX={'17%'} w='60%' border='2px solid red' alignSelf={'center'}>
+            <Image src={'/assets/exballs.svg'} position={'absolute'} right='5' top='5' boxSize={{ base: '42px', md: '100px', lg: '170px' }} />
+            <SimpleGrid columns={2} pt='70px' spacingX={'17%'} w={{ base: '90%', md: '60%' }} border='2px solid red' alignSelf={'center'}>
                 <VStack alignItems={'flex-start'} border='2px solid white' spacing={'51px'}>
-                    <Heading fontWeight={700} color={'white'} fontSize={'32px'}>Education</Heading>
+                    <Heading fontWeight={700} color={'white'} fontSize={{ base: '21px', lg: '33px' }}>Education</Heading>
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
@@ -35,7 +35,7 @@ function Experiences() {
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
                 </VStack>
                 <VStack alignItems={'flex-start'} border='2px solid white' spacing={'51px'}>
-                    <Heading fontWeight={700} color={'white'} fontSize={'32px'}>Jobs</Heading>
+                    <Heading fontWeight={700} color={'white'} fontSize={{ base: '21px', lg: '33px' }}>Experience</Heading>
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
                     <Item date={'January 2020 - December 2023'} title={'Computer Science'} institution={'Universitas Indonesia'} />
