852c5d42b1aa04eee6eff6de456b54ac9ae6a8cb
Dzikri <dzikri.qalam01@ui.ac.id>
2022-09-03 23:28:17 +0700

    feat: update my projects

--- ui/component/Projects/index.jsx
+++ ui/component/Projects/index.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
-import { Text, VStack, Heading, Image, HStack, Box, Button } from '@chakra-ui/react'
+import { Text, VStack, Heading, Image, HStack, Box, Button, Link } from '@chakra-ui/react'
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css"
 import "swiper/css/pagination"
 import { Pagination } from "swiper"
 import { Icon } from '@iconify/react'
 
-function Project() {
+function Project({ title, stacks, link, image, viewCode }) {
     return (
         <VStack bg='#272727' borderRadius={'18px'} spacing='0' boxShadow='0px 4px 40px rgba(0, 0, 0, 0.16)' zIndex='2' w={{ base: '300px', md: '412px' }}>
-            <Image src='/assets/dummy_card.png' />
+            <Image src={image} borderRadius="18px 18px 0 0" />
             <VStack p='25px' align='flex-start' justifyContent={'flex-start'} w='full' borderRadius={'18px'}>
-                <Text fontWeight={700} fontSize='18px' color='white'>
-                    Portfolio Website
+                <Text fontWeight={700} fontSize='18px' color='white' userSelect={'none'}>
+                    {title}
                 </Text>
-                <HStack>
-                    <Icon icon={'akar-icons:github-fill'} width='26.25px' height='26.25px' color='white' />
-                    <Text fontWeight={600} fontSize='14px' color='#E2E2E2'>
-                        UI Design
-                    </Text>
+                <HStack spacing={3}>
+                    {stacks && stacks.map((ctx, idx) => <Icon icon={ctx} key={ctx} width='26.25px' height='26.25px' color='#13FF00' />)}
                 </HStack>
-                <HStack w='full' justifyContent={'space-between'}>
-                    <Text fontWeight={600} fontSize='14px' color='white'>
-                        View Code &rarr;
-                    </Text>
-                    <Button bg='#13FF00'>See Project</Button>
+                <HStack w='full' justifyContent={'space-between'} pt='24px'>
+                    {viewCode !== "" &&
+                        <Link href={viewCode} style={{ textDecoration: 'none' }} isExternal>
+                            <Text fontWeight={600} fontSize='14px' color='white'>
+                                View Code &rarr;
+                            </Text>
+                        </Link>
+                    }
+                    <Link href={link} style={{ textDecoration: 'none' }} isExternal >
+                        <Button bg='#13FF00'>See Project</Button>
+                    </Link>
                 </HStack>
             </VStack>
-        </VStack>
+        </VStack >
     )
 }
 
@@ -40,11 +43,49 @@ function Projects() {
         },
     }
 
+    const allProject = [
+        {
+            title: "Property Agent",
+            stacks: ["akar-icons:react-fill", "fa6-brands:golang"],
+            link: "https://nanyeravictoria.com/",
+            image: "/assets/era_victoria.png",
+            viewCode: ""
+        },
+        {
+            title: "Festival Rakyat UI",
+            stacks: ["akar-icons:gatsby-fill", "ion:logo-nodejs"],
+            link: "https://fesrak-staging.netlify.app/",
+            image: "/assets/fesrak.png",
+            viewCode: ""
+        },
+        {
+            title: "LPJ Online",
+            stacks: ["akar-icons:react-fill", "tabler:brand-django"],
+            link: "https://bemapps.cs.ui.ac.id/LPJ",
+            image: "/assets/lpj-online.png",
+            viewCode: ""
+        },
+        {
+            title: "Best Staff",
+            stacks: ["akar-icons:react-fill", "tabler:brand-django"],
+            link: "https://bemapps.cs.ui.ac.id/best-staff",
+            image: "/assets/best-staff.png",
+            viewCode: ""
+        },
+        {
+            title: "Personal Website",
+            stacks: ["teenyicons:nextjs-outline"],
+            link: "/",
+            image: "/assets/personal-web.png",
+            viewCode: "https://github.com/dzikriqalampacil/personal-website"
+        }
+    ]
+
     return (<>
         <Box bg={'#202020'} minH='100vh' position={'relative'} overflow='hidden' id='projects'>
             <Box boxSize={{ base: '500px', sm: '600px', lg: '800px' }} borderRadius={'50%'} bg='#252525' position={'absolute'} right={'-400'} top={'-400'} />
             <Box boxSize={{ base: '500px', sm: '600px', lg: '800px' }} borderRadius={'50%'} bg='#252525' position={'absolute'} left={'-400'} bottom={'-400'} />
-            <VStack pb='26px' mt='40px'>
+            <VStack pb='26px' mt='60px'>
                 <Heading fontWeight={700} color={'white'} fontSize={'37.5px'} zIndex='2' >
                     PROJECTS
                 </Heading>
@@ -53,15 +94,11 @@ function Projects() {
             <Swiper
                 pagination={pagination}
                 modules={[Pagination]}
-                style={{ paddingBottom: '80px' }}
+                style={{ paddingBottom: '80px', marginTop: '15px' }}
                 centeredSlides={true}
                 centeredSlidesBounds={true}
             >
-                <SwiperSlide style={{ display: 'flex', justifyContent: 'center' }}><Project /></SwiperSlide>
-                <SwiperSlide style={{ border: '2px solid blue', display: 'flex', justifyContent: 'center' }}><Project /></SwiperSlide>
-                <SwiperSlide style={{ border: '2px solid blue', display: 'flex', justifyContent: 'center' }}><Project /></SwiperSlide>
-                <SwiperSlide style={{ border: '2px solid blue', display: 'flex', justifyContent: 'center' }}><Project /></SwiperSlide>
-                <SwiperSlide style={{ border: '2px solid blue', display: 'flex', justifyContent: 'center' }}><Project /></SwiperSlide>
+                {allProject.map((ctx, idx) => <SwiperSlide style={{ display: 'flex', justifyContent: 'center' }} key={ctx.link}><Project title={ctx.title} stacks={ctx.stacks} link={ctx.link} image={ctx.image} viewCode={ctx.viewCode} /></SwiperSlide>)}
             </Swiper>
         </Box>
     </>
