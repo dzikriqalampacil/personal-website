5ba702bce2c99647d30cdff369cd5806cdc54d27
Dzikri <dzikri.qalam01@ui.ac.id>
2022-09-06 23:09:08 +0700

    feat: update favicon

--- ui/component/Navbar/index.jsx
+++ ui/component/Navbar/index.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 function Item({ text, isActive, ...rest }) {
     return (
         <Flex direction={'column'} alignItems="center">
-            {/* {isActive && <Box borderRadius={'50%'} bg={'brand.green'} width='7.1px' height='6.23px' />} */}
             <Text color={'brand.white'} _hover={{ color: 'brand.green', cursor: 'pointer', textShadow: '0px 0px 20px #13FF00' }} fontWeight={'700'} fontSize={'15px'}>{text}</Text>
         </Flex>
     )
