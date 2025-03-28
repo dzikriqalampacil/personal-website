1f47a578ed97e84f1bed201e84f56c44b8e66e5d
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2025-03-29 02:05:05 +0700

    feat: make the overflow hidden x to be on global style

--- ui/theme/index.js
+++ ui/theme/index.js
@@ -1,38 +1,41 @@
 import { extendTheme, theme as base } from "@chakra-ui/react";
 
 const theme = extendTheme({
-    colors: {
-        brand: {
-            green: "#13FF00",
-            black: "#202020",
-            blackless: "#3F3F3F",
-            gray: "#E2E2E2",
-            white: "#FFFFFF",
-        },
+  colors: {
+    brand: {
+      green: "#13FF00",
+      black: "#202020",
+      blackless: "#3F3F3F",
+      gray: "#E2E2E2",
+      white: "#FFFFFF",
     },
-    fonts: {
-        heading: `Poppins, ${base.fonts?.heading}`,
-        body: `Poppins, ${base.fonts?.body}`,
+  },
+  fonts: {
+    heading: `Poppins, ${base.fonts?.heading}`,
+    body: `Poppins, ${base.fonts?.body}`,
+  },
+  components: {
+    Input: {
+      defaultProps: {
+        focusBorderColor: "#13FF00",
+      },
     },
-    components: {
-        Input: {
-            defaultProps: {
-                focusBorderColor: '#13FF00',
-            }
-        },
-        Textarea: {
-            defaultProps: {
-                focusBorderColor: '#13FF00',
-            }
-        }
+    Textarea: {
+      defaultProps: {
+        focusBorderColor: "#13FF00",
+      },
     },
-    styles: {
-        global: {
-            html: {
-                scrollBehavior: 'smooth'
-            }
-        },
+  },
+  styles: {
+    global: {
+      html: {
+        scrollBehavior: "smooth",
+      },
+      body: {
+        overflowX: "hidden",
+      },
     },
+  },
 });
 
-export default theme
+export default theme;
