df869cf000542fcb73334edf339e2769e769c2c4
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2025-03-30 00:53:14 +0700

    feat: update about page

--- ui/component/About/index.jsx
+++ ui/component/About/index.jsx
@@ -14,7 +14,7 @@ function About() {
             </Heading>
             <Box bg='brand.green' height={'3px'} width='65px' borderRadius={'10px'} />
             <Text fontWeight={500} color={'#8F8F8F'} w={{ base: '80%', md: '49%' }} pt='26px' textAlign={'center'} fontSize='16px'>
-                Hello! I'm <Text as='span' color='#13FF00'>Dzikri Qalam Hatorangan</Text> and currently I'm pursuing <Text as='span' color='#13FF00'>Computer Science</Text> degree at <Text as='span' color='#13FF00'>Universitas Indonesia</Text>. I'm passionate about software development especially as <Text as='span' color='#13FF00'>Front-end Engineer </Text> and <Text as='span' color='#13FF00'>Back-end Engineer</Text>. I like to learn more and more about the area, challenges that take me out of my comfort zone are the best.
+                Hello! I'm <Text as='span' color='#13FF00'>Dzikri Qalam Hatorangan</Text> and I'm a <Text as='span' color='#13FF00'>Computer Science</Text> fresh graduate from <Text as='span' color='#13FF00'>Universitas Indonesia</Text>. I'm passionate about software development especially as <Text as='span' color='#13FF00'>Front-end Engineer </Text> and <Text as='span' color='#13FF00'>Back-end Engineer</Text>. I like to learn more and more about the area, challenges that take me out of my comfort zone are the best.
                 <br /><br />
                 I have developed and experienced in <Text as='span' color='#13FF00'>Web Apps</Text>, <Text as='span' color='#13FF00'>Mobile Apps</Text>, <Text as='span' color='#13FF00'>UI Design</Text>, <Text as='span' color='#13FF00'>Computer Program</Text>, <Text as='span' color='#13FF00'>Data Science</Text>, and <Text as='span' color='#13FF00'>Data Analysis</Text>.
             </Text>
